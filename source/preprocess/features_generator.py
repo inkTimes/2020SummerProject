d774993b71753193658b5d684e1dea70db1e416d
inkTimes <67846998+inkTimes@users.noreply.github.com>
2020-07-21 17:46:17 -0500

    Add files via upload

--- source/preprocess/features_generator.py
+++ source/preprocess/features_generator.py
@@ -1,24 +1,29 @@
 import jieba 
 from pypinyin import pinyin 
 
-class features_generator:
+class FeaturesGenerator:
 
 	def __init__(self):
 		self.tokens = []
 		self.pin_yin = []
+		file_route = "/Users/ink/Documents/SummerProject/data/cn_stopwords.txt"
+		self.stopwords = set(line.strip() for line in open(file_route))
 
 	def get_tokens(self, data):
 		self.tokens = []
 		for line in data:
 			seg = jieba.cut(line, cut_all=False)
-			seg = [i.strip() for i in list(seg)]
-			self.tokens += seg 
+			seg = [i for i in list(seg) if i.strip()]
+			seg = [i for i in seg if (i not in self.stopwords)]
+			seg = ' '.join(elem for elem in seg)
+			self.tokens.append(seg)
 		return self.tokens
 
 	def get_pinyin(self, tokens):
 		self.pin_yin = []
 		for token in tokens:
 			py = pinyin(token)
-			for element in py:
-				self.pin_yin += element
+			for elem in py:
+				elem = [i for i in elem if i.strip()]
+				self.pin_yin += elem
 		return self.pin_yin
---
+++ source/preprocess/qa_generator.py
@@ -0,0 +1,60 @@
+import re 
+
+class QAGenerator:
+	'''This class aims to obtain the questions and answers from the original data!'''
+
+	def __init__(self, orig_data):
+
+		self.orig_data = orig_data
+		self.question = []
+		self.answer = []
+		self.ans = False
+
+
+	def processData(self):
+		answer_list = []
+		question_list = []
+		shift = False
+
+		for line in self.orig_data:
+
+			if line[0] == '-':
+				self.ans = False
+				if shift != self.ans:
+					self.answer.append(' '.join([i.strip() for i in answer_list]))
+					answer_list = []
+					shift = self.ans
+				continue
+			else:
+				m = re.search(r"\((\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)\):", line)
+
+				if m != None:
+					line = re.split(r"\):\s+", line)
+					if line[0][0:5] == "佩爱旗舰店":
+						self.ans = True
+						answer_list.append(line[-1])
+					else:
+						self.ans = False
+						question_list.append(line[-1])
+				else:
+					if self.ans == True:
+						answer_list.append(line)
+					else:
+						question_list.append(line)
+			if shift != self.ans:
+				if self.ans == True:
+					if len(question_list) != 0:
+						self.question.append(' '.join([i.strip() for i in question_list]))
+						question_list = []
+				else:
+					if len(answer_list) != 0:
+						self.answer.append(' '.join([i.strip() for i in answer_list]))
+						answer_list = []
+				shift = self.ans
+
+	def get_answers(self):
+		return self.answer
+
+	def get_questions(self):
+		return self.question
+
